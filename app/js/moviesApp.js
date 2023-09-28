eb9f613e0519a228843cd2397dc6a57f76f70794
Dmitry <lowdimasta@gmail.com>
2023-09-28 18:50:28 +0700

    App Version trim adds

--- app/js/moviesApp.js
+++ app/js/moviesApp.js
@@ -37,7 +37,7 @@ const personalMovieDB = {
 
 function rememberMyFilms () {
     for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', '').trim(78),
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
               b = prompt('На сколько оцените его?', '');
     
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
